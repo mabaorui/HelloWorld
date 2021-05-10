97b9b8395f00ca1e2d1903062af2a0b20791d3bc
mabaorui <805950246@qq.com>
2021-05-10 23:06:34 +0800

    第二次提交代码

--- aveCd.py
+++ aveCd.py
@@ -5,12 +5,6 @@ Created on Mon Oct 26 14:16:19 2020
 @author: Administrator
 """
 
-#import dist_chamfer_3D as dist_chamfer_3D
-#import torch
-import numpy as np
-import os
-#import fscore
-import sys
 
 class_id = sys.argv[1]
 thresh = sys.argv[2]
@@ -26,38 +20,7 @@ files = []
 f = open(test_list,'r')
 for index,line in enumerate(f):
     files.append(line.strip().split('/')[1])
-    #files.append(line.strip('\n'))
-f.close()
-#print(len(files))
 
-#f = open('/data1/mabaorui/AtlasNetOwn/data/val.txt','r')
-#for index,line in enumerate(f):
-#    if(line.strip().split('/')[0]==class_id):
-#        #print(line)
-#        files.append(line.strip().split('/')[1])
-#f.close()
-
-#device = torch.device("cuda:0")
-cd2 = 0
-nc = 0
-cd = 0
-num = 0
-print(len(files))
-#for i in range(len(files)):
-for i in range(1310):
-    for j in range(24):
-        #print(OUTPUT_DIR + files[i] + '_0.0'+ '_{}.npz'.format(j))
-        if(os.path.exists(OUTPUT_DIR + files[i] + thresh + '_{}.npz'.format(j))):
-            load_data = np.load(OUTPUT_DIR + files[i] + thresh +  '_{}.npz'.format(j))
-            cd2 = cd2 + load_data['cd2']
-            nc = nc + load_data['nc']
-            cd = cd + load_data['cd']
-            num = num + 1
-    #print(OUTPUT_DIR + files[i] + thresh + '.npz')
-#    if(os.path.exists(OUTPUT_DIR + files[i] + thresh + '.npz')):
-#        load_data = np.load(OUTPUT_DIR + files[i] + thresh + '.npz')
-#        if(load_data['cd2']>0.1):
-#            print(load_data['cd2'])
 #            print(files[i])
 #        cd2 = cd2 + load_data['cd2']
 #        nc = nc + load_data['nc']
